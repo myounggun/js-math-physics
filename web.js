5746951ac1660b998bd4b90e2f8df35c99a9f9b2
myounggun <myounggun@gmail.com>
2014-04-11 15:44:02 +0900

    node.js 환경세팅.

---
+++ web.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
