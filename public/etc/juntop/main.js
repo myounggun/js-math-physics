13d4586cd8b33fddff0ca832d58482b253edff3d
myounggun <myounggun@gmail.com>
2014-07-26 17:19:58 +0900

    bitmap collesion test.

---
+++ public/etc/juntop/main.js
@@ -0,0 +1,38 @@
+/**
+ * twip: 1/20 -> 0.05 단위로 좌표 계산
+ */
+window.onload = function() {
+    var canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d"),
+        w = canvas.width = 200,//window.innerWidth,
+        h = canvas.height = 200;//window.innerHeight;
+    
+    context.fillStyle = '#000000';
+    context.fillText('가나다', 50, 50);
+    
+    var points = [];
+    var imageData = context.getImageData(0, 0, w, h).data;
+    for (var y = 0; y < h; y +=2) {
+        for (var x=0; x < w; x++) {
+            var index = (y * w + x) * 4,
+                alpha = imageData[index + 3];
+            
+            if (alpha > 0) {
+               points.push({ x: x, y: y});
+            }
+        }
+    }
+    
+    points.sort(function(a, b) {
+        if (a.x == b.x) {
+            return a.y - a.y;
+        }
+        return a.x - b.x;
+     });
+    
+    var sp = points[0],
+        ep = points[points.length - 1];
+    
+    context.fillStyle = "#ff0000";
+    context.fillRect(sp.x, sp.y, ep.x - sp.x, ep.y - sp.y);
+};
